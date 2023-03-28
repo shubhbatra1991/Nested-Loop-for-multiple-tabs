240f70763b834a01722408708fc1c743acfc838d
saurabh batra <godwithsam@gmail.com>
2023-03-28 04:21:34 +0100

    added some minor changes.

--- src/components/MyField.js
+++ src/components/MyField.js
@@ -3,7 +3,7 @@ import React from "react";
 const MyField = () => {
     return(
         <div className='head2'>
-            <h1 className="map-all-build-details"> MapAll Build Details </h1> 
+            <h1 className="map-all-build-details"> Nested Loops </h1> 
         </div>
     );
 }
