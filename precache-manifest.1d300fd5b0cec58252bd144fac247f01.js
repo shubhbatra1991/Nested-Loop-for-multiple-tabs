a1d5db65feeb09ef0755d88d6de162b8115efaa0
saurabh batra <godwithsam@gmail.com>
2023-03-28 04:18:36 +0100

    Updates

---
+++ precache-manifest.1d300fd5b0cec58252bd144fac247f01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cc393e6382ee93480d5",
+    "url": "/Nested-Loop-for-multiple-tabs/static/css/main.d546cdee.chunk.css"
+  },
+  {
+    "revision": "3cc393e6382ee93480d5",
+    "url": "/Nested-Loop-for-multiple-tabs/static/js/main.3cc393e6.chunk.js"
+  },
+  {
+    "revision": "ac3a9864d6791bb939c5",
+    "url": "/Nested-Loop-for-multiple-tabs/static/js/1.ac3a9864.chunk.js"
+  },
+  {
+    "revision": "980cdc2d7dfa42c91197",
+    "url": "/Nested-Loop-for-multiple-tabs/static/js/runtime~main.980cdc2d.js"
+  },
+  {
+    "revision": "4d51a477f22bd061cfa5024b6e9a2eee",
+    "url": "/Nested-Loop-for-multiple-tabs/index.html"
+  }
+];
